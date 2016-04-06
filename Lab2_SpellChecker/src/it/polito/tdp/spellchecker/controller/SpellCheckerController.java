1e9dd64f1028dc4831ae829fca4325bf11ee0cb5
LUCA <s205267@studenti.polito.it>
2016-04-06 19:19:52 +0200

    Esercizio lab 2 ultimato

--- Lab2_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab2_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,150 @@
 package it.polito.tdp.spellchecker.controller;
+import java.awt.Color;
+import java.net.URL;
+import java.util.*;
 
+import it.polito.tdp.spellchecker.model.EnglishDictionary;
+import it.polito.tdp.spellchecker.model.ItalianDicionary;
+import it.polito.tdp.spellchecker.model.RichWord;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 public class SpellCheckerController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    private ItalianDicionary italian;
+    private EnglishDictionary inglese;
+    
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<String> cmblingua;
+
+    @FXML
+    private TextArea text1;
+
+    @FXML
+    private Button bttCorreggi;
+
+    
+    @FXML
+    private TextFlow text2;
+    
+    @FXML
+    private Button bttClear;
+
+    @FXML
+    private Label lblres;
+
+    @FXML
+    void doClear(ActionEvent event) {
+    	text1.clear();
+    	text2.getChildren().clear();
+    	lblres.setText("");
+    }
+
+    @FXML
+    void doSpellCheck(ActionEvent event) {
+		List<String> parole = new LinkedList<String>();
+		List<RichWord> risultato = new LinkedList<RichWord>();
+		String visualizza = "";
+		String lingua = cmblingua.getValue();
+		long l1=System.nanoTime();
+		long l2=0;
+		
+    	if(text1!= null)
+    	{
+    		if(lingua!=null)
+    		{	
+	    		if(lingua.compareTo("Italiano")==0)
+	    		{
+		    		italian.loadDictionary();
+		    		String inserita = text1.getText();
+		    		StringTokenizer st = new StringTokenizer(inserita);
+		    		while( st.hasMoreTokens())
+		    		{
+		    			String token = st.nextToken().toLowerCase().trim();
+		    			parole.add(token);
+		    		}
+		    		
+		    		risultato = italian.spellCheckText(parole);
+		    		for(RichWord r : risultato)
+		    		{
+		
+		    			if(r.getStato()== true)
+		    			{
+		    			visualizza += r.getParola()+"\n";
+		    			}
+		    			else{}
+		    		}
+		    	l2=System.nanoTime();
+		    	}
+	    	
+	    		else if(lingua.compareTo("English")==0)
+				{
+		    		inglese.loadDictionary();
+		    		String inserita = text1.getText();
+		    		StringTokenizer st = new StringTokenizer(inserita);
+		    		while( st.hasMoreTokens())
+		    		{
+		    			String token = st.nextToken().toLowerCase().trim();
+		    			parole.add(token);
+		    		}
+		    		
+		    		risultato = inglese.spellCheckText(parole);
+		    		for(RichWord r : risultato)
+		    		{
+		
+		    			if(r.getStato()== true)
+		    			{
+		    			visualizza += r.getParola()+"\n";
+		    			}
+		    			else{}
+		    		}
+		    		l2=System.nanoTime();
+		    	}
+    		}
+	    	else
+	    	{
+	    		lblres.setText("Selezionare una lingua");
+	    		return ;
+	    	}
+    	}
+
+		this.aggiornaRisultato(visualizza, l2-l1);    	
+
+}
+    public void aggiornaRisultato(String res,long secondi)
+    {
+		 Text casella= new Text();
+		 casella.setText(res);
+		 casella.setFill(javafx.scene.paint.Color.RED);
+		lblres.setText("Correzione completata in "+(secondi)/(1e9)+"secondi");
+		text2.getChildren().add(casella);
+    }
+    
+    
+    public void setModel( ItalianDicionary ita, EnglishDictionary eng){
+    	italian = ita;
+    	inglese= eng;
+    	}
+
+    @FXML
+    void initialize() {
+        assert cmblingua != null : "fx:id=\"cmblingua\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert text1 != null : "fx:id=\"text1\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert bttCorreggi != null : "fx:id=\"bttCorreggi\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert bttClear != null : "fx:id=\"bttClear\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert text2 != null : "fx:id=\"text2\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert lblres != null : "fx:id=\"lblres\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        text2.setVisible(true);
+    }
 }
